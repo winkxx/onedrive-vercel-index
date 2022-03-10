d125c4afa6c5d037fc02a8f8c3cc17b83e0e59cd
winkxx <68945988+winkxx@users.noreply.github.com>
2022-03-10 20:40:18 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -61,5 +61,5 @@ module.exports = {
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
-  datetimeFormat: '2022-03-10 20:40:10',
+  datetimeFormat: '2022-03-10 20:40:8',
 }
